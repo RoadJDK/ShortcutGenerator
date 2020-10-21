ced666c8c702feb262be379ef7b4ac6e0e7b0722
RoadJDK <58588256+RoadJDK@users.noreply.github.com>
2020-10-21 13:55:31 +0200

    added project

---
+++ ShortcutGenerator/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ShortcutGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ckbxIcon = new System.Windows.Forms.CheckBox();
+            this.txtInputName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtInputShortcut = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(223, 326);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Shortcut generator";
+            // 
+            // ckbxIcon
+            // 
+            this.ckbxIcon.AutoSize = true;
+            this.ckbxIcon.Location = new System.Drawing.Point(223, 190);
+            this.ckbxIcon.Name = "ckbxIcon";
+            this.ckbxIcon.Size = new System.Drawing.Size(89, 17);
+            this.ckbxIcon.TabIndex = 4;
+            this.ckbxIcon.Text = "custom icon?";
+            this.ckbxIcon.UseVisualStyleBackColor = true;
+            this.ckbxIcon.CheckStateChanged += new System.EventHandler(this.ckbxIcon_CheckStateChanged);
+            // 
+            // txtInputName
+            // 
+            this.txtInputName.Location = new System.Drawing.Point(223, 252);
+            this.txtInputName.Name = "txtInputName";
+            this.txtInputName.Size = new System.Drawing.Size(100, 20);
+            this.txtInputName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(220, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "shortcut Name";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(221, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "shortcut";
+            // 
+            // txtInputShortcut
+            // 
+            this.txtInputShortcut.Location = new System.Drawing.Point(223, 122);
+            this.txtInputShortcut.Name = "txtInputShortcut";
+            this.txtInputShortcut.Size = new System.Drawing.Size(100, 20);
+            this.txtInputShortcut.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtInputShortcut);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtInputName);
+            this.Controls.Add(this.ckbxIcon);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox ckbxIcon;
+        private System.Windows.Forms.TextBox txtInputName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtInputShortcut;
+    }
+}
+
---
+++ ShortcutGenerator/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using WSShell = IWshRuntimeLibrary.WshShell;
+using WSShortcut = IWshRuntimeLibrary.IWshShortcut;
+
+namespace ShortcutGenerator
+{
+    public partial class Form1 : Form
+    {
+
+        String linkName;
+        String iconPath;
+        bool checkbox;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void ckbxIcon_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (ckbxIcon.Checked)
+            {
+                openFileDialog1.ShowDialog();
+                iconPath = openFileDialog1.FileName;
+                checkbox = true;
+            }
+            else
+            {
+                checkbox = true;
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+            linkName = txtInputName.Text;
+            createShortcut(linkName, iconPath);
+        }
+
+        private void createShortcut(String link, String path)
+        {
+            string LNKF = System.Environment.GetFolderPath
+
+            (System.Environment.SpecialFolder.DesktopDirectory)
+            + "\\" + link + ".lnk";
+
+            WSShell wsho = new WSShell();
+            WSShortcut sc = (WSShortcut)wsho.CreateShortcut(LNKF);
+
+            sc.TargetPath = txtInputShortcut.Text;
+            sc.Description = Application.ProductName + " ausführen";
+            sc.WorkingDirectory = Application.ProductName;
+            if (checkbox == true)
+            {
+                sc.IconLocation = iconPath;
+            }
+
+            sc.Save();
+        }
+
+    }
+}
